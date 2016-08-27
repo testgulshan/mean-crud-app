426b12c3480c4012694ff9ad4547d04a81619ffa
Gulshan <test.gulshan@gmail.com>
2016-08-28 00:50:54 +0530

    Added get routing.

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
 var port            = 3000;
 
+var mongoose        = require('mongoose');
+    mongoose.connect('mongodb://localhost/mean-auth');
+
+// error handling
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to Database: "mean-auth"');
+});
+
 app.use(bodyParser.json());
 
 app.use(methodOverride('X-HTTP-Method-Override'))
@@ -11,8 +21,42 @@ app.use(methodOverride('X-HTTP-Method-Override'))
 app.use(express.static(__dirname + '/public'));
 
 
+// schema definition
+var userSchema = mongoose.Schema({
+  name: String
+});
+
+// create a modal with the userSchema schema
+var User = mongoose.model('users', userSchema);
+
 app.get('/', function (req, res) {
   res.send('index.html');
+});
+
+// get all users
+app.get('/users', function (req, res) {
+  User.find({})
+    .exec(function (err, users) {
+      if (err) {
+        res.send('error has occurred')
+      } else {
+        res.json(users)
+      }
+    });
+});
+
+// get specific user via id
+app.get('/users/:id', function (req, res) {
+  User.findOne({
+    _id: req.params.id
+  })
+  .exec(function (err, user) {
+    if (err) {
+      res.send('error occurred')
+    } else {
+      res.json(user);
+    }
+  })
 });
 
 app.listen(port, function () {
